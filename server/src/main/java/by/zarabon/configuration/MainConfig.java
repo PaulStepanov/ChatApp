1a144b497477845c7b6dcd4ebe35bc9ec07c27a5
Павел <freestalker1987@gmail.com>
2017-04-03 19:29:39 +0300

    init server

---
+++ server/src/main/java/by/zarabon/configuration/MainConfig.java
@@ -0,0 +1,29 @@
+package by.zarabon.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebMvc
+@EnableJms
+@EnableWebSocketMessageBroker
+public class MainConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/queue");
+        config.setApplicationDestinationPrefixes("/message");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+        stompEndpointRegistry.addEndpoint("/gs-guide-websocket").withSockJS();
+    }
+}
---
+++ server/src/main/java/by/zarabon/controllers/MessageController.java
@@ -0,0 +1,15 @@
+package by.zarabon.controllers;
+
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MessageController {
+
+    //Fetching message from user
+    @RequestMapping(path = "/message/send")
+    public
+
+
+}
